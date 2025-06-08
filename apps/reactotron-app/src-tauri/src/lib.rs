51b408b4b537c05d80167c92299f29c5c60df542
ohah <bookyoon173@gmail.com>
2025-06-09 05:06:14 +0900

    feat(reactotron-app): tauri-dev-setting

---
+++ apps/reactotron-app/src-tauri/src/lib.rs
@@ -0,0 +1,27 @@
+use tauri::Manager;
+use tauri_plugin_store::StoreExt;
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_os::init())
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                let window = app.get_webview_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
